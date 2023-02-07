5746ccd5234e297a4572e53056b1323d6a8f6f7e
Genyus <genyus@gmail.com>
2023-02-07 20:21:14 +0000

    style: applies Prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const fs = require('fs');
-const express = require('express');
+require("dotenv").config();
+const fs = require("fs");
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 8080;
-const bodyParser = require('body-parser');
-const { v4: uuidv4 } = require('uuid');
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
 const todoFilePath = process.env.BASE_JSON_PATH;
 
 //Read todos from todos.json into variable
@@ -25,7 +25,7 @@ app.get("/", (_, res) => {
   res.status(501).end();
 });
 
-app.get('/todos', (_, res) => {
+app.get("/todos", (_, res) => {
   /*
   res.header("Content-Type","application/json");
   res.sendFile(todoFilePath, { root: __dirname });
@@ -48,7 +48,7 @@ app.get('/todos', (_, res) => {
 //Add DELETE request with path '/todos/:id
 
 app.listen(port, function () {
-    console.log(`Node server is running... http://localhost:${port}`);
+  console.log(`Node server is running... http://localhost:${port}`);
 });
 
 module.exports = app;
