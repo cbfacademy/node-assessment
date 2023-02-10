be3d9bce707f1d0af7d3cbd664a4c86d3030c62c
Genyus <genyus@gmail.com>
2023-02-10 03:34:31 +0000

    chore: removes poorly-implemented todo loading

--- src/app.js
+++ src/app.js
@@ -7,9 +7,6 @@ const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 const todoFilePath = process.env.BASE_JSON_PATH;
 
-// Read todos from todos.json into variable
-const getTodos = () => require(path.join(__dirname, todoFilePath));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.raw());
